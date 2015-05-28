28e4f36da28395ad87f3b7de8b78d5fe91b753c0
stefanRitter <stefan@stefanritter.com>
2015-05-28 17:53:04 +0100

    test UrlProcessor

--- server/utils/__tests__/UrlProcessor-test.js
+++ server/utils/__tests__/UrlProcessor-test.js
@@ -12,8 +12,24 @@ var beforeEach = lab.beforeEach;
 var after = lab.after;
 var expect = Code.expect;
 
+var mockHtml = '<html><head><title>testing title</title></head><body>some body</body></html>';
+
 
 describe('UrlProcessor', function () {
+  var UrlProcessor = require('../UrlProcessor');
+
+  it('should extract the title from the HTML', function (done) {
+    UrlProcessor.request = function (request, callback) {
+      callback(false, {statusCode: 200, request: {href: 'www.test.com'}}, mockHtml);
+    };
+
+    UrlProcessor.process([{url: 't.co/test'}], function (err, links) {
+      expect(err).to.be.null();
+      expect(links.length).to.equal(1);
+      expect(links[0].title).to.equal('testing title');
+      done();
+    });
+  });
 
 
 });
